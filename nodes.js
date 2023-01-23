7f4ef20a56c826d7cfc5d0faf8799497dab84e1c
Exp <17885980+exp111@users.noreply.github.com>
2023-01-23 18:34:23 +0100

    switches

--- nodes.js
+++ nodes.js
@@ -1,16 +1,20 @@
 function getActionText(action) {
     var ret = "";
     console.log("Action: " + action.$type);
-    if (action.$type == "NodeCanvas.Tasks.Actions.QuestAction_AddLogEntry") {
-        ret += "AddLogEntry: ";
-        ret += action.statement._text;
-    } else if (action.$type == "NodeCanvas.Tasks.Actions.SetBoolean") {
-        ret += "SetBoolean: ";
-        ret += action.boolVariable._name;
-        ret += " = "
-        ret += action.setTo ?? "True"; // defaults to true
-    } else {
-        ret = action.$type;
+    switch (action.$type) {
+        case "NodeCanvas.Tasks.Actions.QuestAction_AddLogEntry":
+            ret += "AddLogEntry: ";
+            ret += action.statement._text;
+            break;
+        case "NodeCanvas.Tasks.Actions.SetBoolean":
+            ret += "SetBoolean: ";
+            ret += action.boolVariable._name;
+            ret += " = "
+            ret += action.setTo ?? "True"; // defaults to true
+            break;
+        default:
+            ret = action.$type;
+            break;
     }
     return ret;
 }
@@ -18,20 +22,23 @@ function getActionText(action) {
 function getNodeText(node) {
     var ret = "";
     console.log("Node: " + node.$type);
-    if (node.$type == "NodeCanvas.StateMachines.QuestStep") {
-        ret = "QuestStep: \n";
-        let list = node._actionList;
-        if (list != null) {
-            let execution = list.executionMode == "ActionsRunInParallel" ? "Parallel" : "Sequence"
-            ret += "Execution: In" + execution + "\n";
-            let actions = list.actions;
-            for (let i = 0; i < actions.length; i++) {
-                let action = actions[i];
-                ret += "- " + getActionText(action) + "\n";
-            };
-        }
-    } else {
-        ret += node.$type;
+    switch (node.$type) {
+        case "NodeCanvas.StateMachines.QuestStep":
+            ret = "QuestStep: \n";
+            let list = node._actionList;
+            if (list != null) {
+                let execution = list.executionMode == "ActionsRunInParallel" ? "Parallel" : "Sequence"
+                ret += "Execution: In" + execution + "\n";
+                let actions = list.actions;
+                for (let i = 0; i < actions.length; i++) {
+                    let action = actions[i];
+                    ret += "- " + getActionText(action) + "\n";
+                };
+            }
+            break;
+        default:
+            ret += node.$type;
+            break;
     }
     return ret;
 }
@@ -39,35 +46,40 @@ function getNodeText(node) {
 function getConditionText(condition) {
     var ret = "";
     console.log("Condition: " + condition.$type);
-    if (condition.$type == "NodeCanvas.Framework.ConditionList") {
-        let list = condition.conditions;
-        ret += "Conditions: \n";
-        if (list != null) {
-            for (let i = 0; i < list.length; i++) {
-                let subCondition = list[i];
-                ret += "- " + getConditionText(subCondition) + "\n";
-            };
-        }
-    } else if (condition.$type == "NodeCanvas.Tasks.Conditions.Condition_QuestEventOccured") {
-        ret += "QuestEventOccured: ";
-        // transform uid into name
-        let event = Global.events[condition.QuestEventRef.m_eventUID];
-        if (event)
-            ret += `${event.Name} (${condition.QuestEventRef.m_eventUID})`;
-        else
-            ret += condition.QuestEventRef.m_eventUID;
-    } else if (condition.$type == "NodeCanvas.Tasks.Conditions.CheckBoolean") {
-        ret += "CheckBoolean: ";
-        ret += condition.valueA._name;
-        ret += " == "
-        if (condition.valueB == undefined)
-            ret += "True"
-        else if (condition.ValueB._value == undefined) //TODO: seems like a false value isnt serialized??
-            ret += "False"
-        else
-            ret += condition.valueB._value;
-    } else {
-        ret = condition.$type;
+    switch (condition.$type) {
+        case "NodeCanvas.Framework.ConditionList":
+            let list = condition.conditions;
+            ret += "Conditions: \n";
+            if (list != null) {
+                for (let i = 0; i < list.length; i++) {
+                    let subCondition = list[i];
+                    ret += "- " + getConditionText(subCondition) + "\n";
+                };
+            }
+            break;
+        case "NodeCanvas.Tasks.Conditions.Condition_QuestEventOccured":
+            ret += "QuestEventOccured: ";
+            // transform uid into name
+            let event = Global.events[condition.QuestEventRef.m_eventUID];
+            if (event)
+                ret += `${event.Name} (${condition.QuestEventRef.m_eventUID})`;
+            else
+                ret += condition.QuestEventRef.m_eventUID;
+            break;
+        case "NodeCanvas.Tasks.Conditions.CheckBoolean":
+            ret += "CheckBoolean: ";
+            ret += condition.valueA._name;
+            ret += " == "
+            if (condition.valueB == null)
+                ret += "True"
+            else if (condition.valueB._value == null) //TODO: seems like a false value isnt serialized??
+                ret += "False"
+            else
+                ret += condition.valueB._value;
+            break;
+        default:
+            ret = condition.$type;
+            break;
     }
     return ret;
 }
@@ -75,14 +87,17 @@ function getConditionText(condition) {
 function getEdgeText(edge) {
     var ret = "";
     console.log("Edge: " + edge.$type);
-    if (edge.$type == "NodeCanvas.StateMachines.FSMConnection") {
-        ret = "FSMConnection: \n";
-        let condition = edge._condition;
-        if (condition) {
-            ret += getConditionText(condition);
-        }
-    } else {
-        ret += edge.$type;
+    switch (edge.$type) {
+        case "NodeCanvas.StateMachines.FSMConnection":
+            ret = "FSMConnection: \n";
+            let condition = edge._condition;
+            if (condition) {
+                ret += getConditionText(condition);
+            }
+            break;
+        default:
+            ret += edge.$type;
+            break;
     }
     return ret;
 }
